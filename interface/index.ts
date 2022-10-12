fb0b456766f5f72b1e4c2f790655d3a2a3d2c993
didim1 <dimasseptian.ns@gmail.com>
2022-10-13 01:12:17 +0700

    initial

--- interface/index.ts
+++ interface/index.ts
@@ -3,7 +3,7 @@ export const getUrl = () => {
     return process.env.NEXT_PUBLIC_SITE;
   }
 
-  return process.env.NEXT_PUBLIC_VERCEL_URL;
+  return process.env.VERCEL_URL;
 };
 
 export interface Data {
